c880a1ed10fc4a97163ec3dd4e4e41a411156bae
DecisionTree <121787369+DecisionTreee@users.noreply.github.com>
2023-01-04 21:44:08 +0800

    Add files via upload

---
+++ UI/UIUtils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace BiliDownUI.UI
+{
+    internal class UIUtils
+    {
+        public static void SetPage(ContentControl control, Page page)
+        {
+            control.Content = new Frame()
+            {
+                Content = page
+            };
+        }
+    }
+}
